0ece3374ee016a2975292585ceaa95fe230353f0
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2021-09-19 23:48:30 -0400

    build: setup tailwind (#10)
    * Adds tailwindcss to dependencies and initializes
    
    * Replaces global styles with Tailwind directives
    
    * Imports Rubik font, adds fontFamily to tailwind config
    
    * Update tailwind.config.js
    
    Co-authored-by: Saihajpreet Singh <saihajpreet.singh@gmail.com>
    
    * adds newline
    
    * actually adds new line
    
    Co-authored-by: Saihajpreet Singh <saihajpreet.singh@gmail.com>

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: [
+    './src/**/*.{js,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'sans': ['Rubik', ...defaultTheme.fontFamily.sans],
+      'serif': [...defaultTheme.fontFamily.serif],
+      'mono': [...defaultTheme.fontFamily.mono],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
