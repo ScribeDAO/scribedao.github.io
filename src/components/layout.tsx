2249a4e87f06eabb76f919b33eaec976a16a6e63
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2021-09-22 12:06:34 -0400

    Adds Layout component and begins grid structure (#11)
    * Adds Layout component and begins grid structure
    
    * Adds newline
    
    * Removes unnecessary config, interpolates site title on layout, removes superfluous type
    
    * Update src/components/layout.tsx
    
    Co-authored-by: Saihajpreet Singh <saihajpreet.singh@gmail.com>
    
    Co-authored-by: Saihajpreet Singh <saihajpreet.singh@gmail.com>

---
+++ src/components/layout.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+export default function Layout({ children, title = 'ScribeDAO' }:{children: React.ReactNode,title?: string}) {
+  return (
+    <div className="grid grid-rows-3">
+      <Head>
+      <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="We summarize crypto articles that you don't have time to read entirely." />
+      </Head>
+      <header>
+        ScribeDAO
+      </header>
+      <main>
+        {children}
+      </main>
+      <footer>
+        Footer
+      </footer>
+    </div>
+  )
+}
