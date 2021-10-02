1f61e7a48f5a2dfa8c5fb17162e5575b01509df4
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2021-10-01 23:56:49 -0400

    Adds mobile menu button and top of da mornin placeholder :hat: mobile nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,14 @@
-import React, { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
-import { ChevronDownIcon, MenuIcon } from '@heroicons/react/solid'
+import React, { Fragment, useState } from 'react'
+import { Dialog, Popover, Transition } from '@headlessui/react'
+import { ChevronDownIcon, MenuIcon, XIcon } from '@heroicons/react/solid'
 import Image from 'next/image'
 import CustomLink from './customLink'
 
 import { HEADER_NAV_LIST } from '../data/constants'
 
 function Nav() {
+  const [open, setOpen] = useState()
+
   return (
     <Popover>
       <section className="lg:container lg:mx-auto lg:px-20 xl:px-28 px-4 sm:px-8 md:px-18 my-5">
@@ -21,12 +23,7 @@ function Nav() {
               />
             </CustomLink>
           </section>
-          <div className="-mr-2 -my-2 md:hidden">
-            <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-              <span className="sr-only">Open menu</span>
-              <MenuIcon className="h-6 w-6" aria-hidden="true" />
-            </Popover.Button>
-          </div>
+
           {/* lg nav bar */}
           <Popover.Group
             as="nav"
@@ -74,11 +71,73 @@ function Nav() {
               )
             )}
           </Popover.Group>
+
+          {/* mobile view hamburger menu */}
+          <div className="-mr-2 -my-2 md:hidden">
+            <Popover.Button
+              onClick={() => setOpen(true)}
+              className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <span className="sr-only">Open menu</span>
+              <MenuIcon className="h-6 w-6" aria-hidden="true" />
+            </Popover.Button>
+          </div>
         </section>
       </section>
 
       {/* mobile nav links :open */}
       {/* <section className="md:hidden">x</section> */}
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog
+          as="aside"
+          className="fixed inset-0 overflow-hidden"
+          onClose={setOpen}>
+          <div className="absolute inset-0 overflow-hidden">
+            {/* grey overlay */}
+            <Transition.Child
+              as={Fragment}
+              enter="ease-in-out duration-500"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in-out duration-500"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0">
+              <Dialog.Overlay className="absolute inset-0 bg-gray-100 bg-opacity-50 transition opacity" />
+            </Transition.Child>
+
+            <div className="fixed inset-y-0 right-0 max-w-full flex">
+              <Transition.Child>
+                <div className="relative w-screen max-w-md">
+                  <Transition.Child>
+                    <div className="absolute top-0 left-0 -ml-8 pt-4 pr-2 flex sm:-ml-10 sm:pr-4">
+                      <button
+                        type="button"
+                        className="rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                        onClick={() => setOpen(false)}>
+                        <span className="sr-only">Close panel</span>
+                        <XIcon className="h-6 w-6" aria-hidden="true" />
+                      </button>
+                    </div>
+                  </Transition.Child>
+                  <div className="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll">
+                    <div className="px-4 sm:px-6">top of da menu</div>
+                    <div className="mt-6 relative flex-1 px-4 sm:px-6 grid">
+                      {HEADER_NAV_LIST.map(({ title, href, sublinks }) => (
+                        <CustomLink key={title}>{title}</CustomLink>
+                      ))}
+                      <div className="absolute inset-0 px-4 sm:px-6">
+                        <div
+                          className="h-full border-2 border-gray-200"
+                          aria-hidden="true"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
       <hr />
     </Popover>
   )
