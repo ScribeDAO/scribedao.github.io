ce129723bf2da1aba5e0c7db22da97413f91a575
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2021-10-02 16:41:12 -0400

    Adds nested menu dropdown, closing on clicks

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react'
-import { Dialog, Popover, Transition } from '@headlessui/react'
+import { Dialog, Menu, Popover, Transition } from '@headlessui/react'
 import { ChevronDownIcon, MenuIcon, XIcon } from '@heroicons/react/solid'
 import Image from 'next/image'
 import CustomLink from './customLink'
@@ -7,10 +7,12 @@ import CustomLink from './customLink'
 import { HEADER_NAV_LIST } from '../data/constants'
 
 function Nav() {
-  const [open, setOpen] = useState()
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false)
 
   return (
-    <Popover>
+    <nav>
       <section className="lg:container lg:mx-auto lg:px-20 xl:px-28 px-4 sm:px-8 md:px-18 my-5">
         <section className="flex justify-between items-center">
           <section>
@@ -25,7 +27,7 @@ function Nav() {
           </section>
 
           {/* lg nav bar */}
-          <Popover.Group
+          <Popover
             as="nav"
             className="hidden md:flex items-baseline text-black space-x-8 md:text-base xl:text-lg">
             {HEADER_NAV_LIST.map(({ title, href, sublinks }) =>
@@ -39,6 +41,7 @@ function Nav() {
                       </Popover.Button>
 
                       <Transition
+                        show={open}
                         as={Fragment}
                         enter="transition duration-200 ease-out"
                         enterFrom="opacity-0 translate-y-1"
@@ -70,27 +73,31 @@ function Nav() {
                 </CustomLink>
               )
             )}
-          </Popover.Group>
+          </Popover>
 
           {/* mobile view hamburger menu */}
-          <div className="-mr-2 -my-2 md:hidden">
-            <Popover.Button
-              onClick={() => setOpen(true)}
-              className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+          <Popover
+            as="div"
+            className="-mr-2 -my-2 md:hidden"
+            onClick={() => {
+              setIsMobileMenuOpen(true)
+              console.log('clicked!')
+            }}>
+            <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
               <span className="sr-only">Open menu</span>
               <MenuIcon className="h-6 w-6" aria-hidden="true" />
             </Popover.Button>
-          </div>
+          </Popover>
         </section>
       </section>
 
       {/* mobile nav links :open */}
       {/* <section className="md:hidden">x</section> */}
-      <Transition.Root show={open} as={Fragment}>
+      <Transition show={isMobileMenuOpen} as={Fragment}>
         <Dialog
           as="aside"
           className="fixed inset-0 overflow-hidden"
-          onClose={setOpen}>
+          onClose={setIsOpen}>
           <div className="absolute inset-0 overflow-hidden">
             {/* grey overlay */}
             <Transition.Child
@@ -103,43 +110,75 @@ function Nav() {
               leaveTo="opacity-0">
               <Dialog.Overlay className="absolute inset-0 bg-gray-100 bg-opacity-50 transition opacity" />
             </Transition.Child>
-
+            {/* mobile menu */}
             <div className="fixed inset-y-0 right-0 max-w-full flex">
               <Transition.Child>
                 <div className="relative w-screen max-w-md">
-                  <Transition.Child>
-                    <div className="absolute top-0 left-0 -ml-8 pt-4 pr-2 flex sm:-ml-10 sm:pr-4">
-                      <button
-                        type="button"
-                        className="rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
-                        onClick={() => setOpen(false)}>
-                        <span className="sr-only">Close panel</span>
-                        <XIcon className="h-6 w-6" aria-hidden="true" />
-                      </button>
-                    </div>
-                  </Transition.Child>
-                  <div className="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll">
-                    <div className="px-4 sm:px-6">top of da menu</div>
-                    <div className="mt-6 relative flex-1 px-4 sm:px-6 grid">
-                      {HEADER_NAV_LIST.map(({ title, href, sublinks }) => (
-                        <CustomLink key={title}>{title}</CustomLink>
-                      ))}
-                      <div className="absolute inset-0 px-4 sm:px-6">
-                        <div
-                          className="h-full border-2 border-gray-200"
-                          aria-hidden="true"
-                        />
-                      </div>
-                    </div>
+                  <div className="absolute top-0 left-0 -ml-8 pt-4 pr-2 flex sm:-ml-10 sm:pr-4">
+                    <button
+                      type="button"
+                      className="rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                      onClick={() =>
+                        setIsMobileMenuOpen(
+                          (isMobileMenuOpen) => !isMobileMenuOpen
+                        )
+                      }>
+                      <span className="sr-only">Close panel</span>
+                      <XIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
                   </div>
+                  <section className="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll">
+                    <div className="px-4 sm:px-6">top of da menu</div>
+                    <div className="mt-6 relative flex-1 px-4 sm:px-6 grid grid-cols-1">
+                      {HEADER_NAV_LIST.map(({ title, href, sublinks }) =>
+                        sublinks ? (
+                          <Menu
+                            show={isOpen}
+                            as="div"
+                            key={title}
+                            className="relative">
+                            {({ open }) => (
+                              <>
+                                <Menu.Button className="flex items-center justify-between my-2">
+                                  <span>{title}</span>
+                                  <ChevronDownIcon className="h-4 w-4" />
+                                </Menu.Button>
+                                {open && (
+                                  <Transition.Child
+                                    show={open}
+                                    as={Fragment}
+                                    enter="transition duration-200 ease-out"
+                                    enterFrom="opacity-0 translate-y-1"
+                                    enterTo="opacity-100 translate-y-0"
+                                    leave="transition ease-in duration-150"
+                                    leaveFrom="opacity-100 translate-y-0"
+                                    leaveTo="opacity-0 translate-y-1">
+                                    <Menu.Items className="transform">
+                                      <div>
+                                        <Menu.Item as="section">Link</Menu.Item>
+                                      </div>
+                                    </Menu.Items>
+                                  </Transition.Child>
+                                )}
+                              </>
+                            )}
+                          </Menu>
+                        ) : (
+                          <CustomLink key={title} href={href} className="my-2">
+                            <p>{title}</p>
+                          </CustomLink>
+                        )
+                      )}
+                    </div>
+                  </section>
                 </div>
               </Transition.Child>
             </div>
           </div>
         </Dialog>
-      </Transition.Root>
+      </Transition>
       <hr />
-    </Popover>
+    </nav>
   )
 }
 
