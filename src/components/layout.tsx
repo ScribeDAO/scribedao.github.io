2ed73be884413b63abb8a0b14c37b62a4d338f43
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-10-30 09:21:26 -0500

    make eslint happy

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
 
-import Navbar from './navbar/'
-import Footer from './footer'
-
 export default function Layout({
   children,
   title = 'ScribeDAO'
@@ -21,11 +18,9 @@ export default function Layout({
           content="We summarize crypto articles that you don't have time to read entirely."
         />
       </Head>
-      {/* <Navbar /> */}
       <main className="lg:container lg:mx-auto lg:px-20 xl:px-28 px-4 sm:px-8 md:px-18">
         {children}
       </main>
-      {/* <Footer /> */}
     </div>
   )
 }
